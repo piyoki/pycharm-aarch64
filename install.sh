18d6e8b486386b75323c22a2cd44fda96c7acfbe
mglovers123 <71198980+mglovers123@users.noreply.github.com>
2021-08-17 08:52:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo " for root user "
 cd ~
+apt-get install sudo
 sudo apt-get update && sudo apt-get install openjdk-14-jdk
 wget https://download.jetbrains.com/python/pycharm-community-2020.3.tar.gz?_gl=1*17r1ves*_ga*MTI3MTkxOTgyOC4xNjI4MzUzMDE0*_ga_V0XZL7QHEB*MTYyOTE0OTQzOC4xMS4xLjE2MjkxNDk0NDguNTA.&_ga=2.153760376.1775587489.1629054795-1271919828.1628353014
 tar -xzf pycharm-community-2020.3.tar.gz && cd pycharm-community-2020.3/bin
